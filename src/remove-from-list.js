10b767aee55b0d50618ac08d7b7da34bb4c174ff
conceptroot <conceptroot@gmail.com>
2023-04-16 21:43:30 +0530

    feat: Remove from list done ✅

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,11 +22,59 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function removeKFromList(l, k) {
+  console.log("converted to array", convertListToArray(l))
+  let base = getNext(l)
+
+  function getNext(current) {
+    let baza = current
+    if (current.value === k) {
+      if (current.next) {
+        current = getNext(current.next)
+        return current
+      } else {
+        current = null
+        return current
+      }
+    } else if (current.next) {
+      current.next = getNext(current.next)
+    } else {
+      current.next = null
+    }
+    return baza
+  }
+
+  console.log('base:', base)
+  console.log("converted base to array", convertListToArray(base))
+  return base
+}
+function convertListToArray(l) {
+  let arr = []
+  while (l.next) {
+    arr.push(l.value)
+    l = l.next
+  }
+  arr.push(l.value)
+  return arr
 }
 
+
+function convertArrayToList(arr) {
+  return arr.reverse().reduce((acc, cur) => {
+    if (acc) {
+      const node = new ListNode(cur);
+      node.next = acc;
+      return node;
+    }
+
+    return new ListNode(cur);
+  }, null);
+}
 module.exports = {
   removeKFromList
 };
+
+
+
+// console.log(removeKFromList(convertArrayToList([3, 1, 2, 3, 4, 5]), 3), [1, 2, 4, 5])  
+// console.log(removeKFromList(convertArrayToList([1, 2, 3]), 3), [1, 2])  
